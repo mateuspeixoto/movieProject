4d2165499fc7658e8e862f58602cc45878262dd9
Mateus Peixoto <mateuspeixoto.contato@gmail.com>
2024-04-17 17:12:41 -0300

    corrigindo bug de routes era uma ''

--- movie-project/src/app/app.routes.ts
+++ movie-project/src/app/app.routes.ts
@@ -1,13 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MovieComponent } from './movie/movie.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [
-    // Rota raiz que redireciona para o login se o usuário não estiver autenticado
+export const routes: Routes = [ 
     {path: '', redirectTo: 'login', pathMatch: 'full'},
-    {path: 'login, component: LoginComponent'},
+    {path: 'login', component: LoginComponent},
     {path: 'home', component: HomeComponent},
     {path: 'movie', component: MovieComponent},
-    // Rota curinga que captura URLs não mapeadas e redireciona para o login
     {path: '**', redirectTo: 'login'}
 ];
